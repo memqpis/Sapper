837297724abcc0509d45cfb137498625607cb177
memqpis <146806475+Daniil228Kharlamov@users.noreply.github.com>
2024-02-13 07:40:54 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -22,22 +22,26 @@ const backgroundMusic = document.getElementById('backgroundMusic')
 const volumeBtn = document.getElementById('volumeBtn')
 const volumeUpImg = document.getElementById('volumeUpImg')
 const volumeMuteImg = document.getElementById('volumeMuteImg')
-
+const titleGameOver = document.getElementById('titleGameOver')
+const logOutImg = document.getElementById('logOutImg')
+const repeatImg = document.getElementById('repeatImg')
+const btnContainer = document.getElementById('btnContainer')
 
 backgroundMusic.volume = 0.07
 let playMusic = false
+
 volumeBtn.onclick = () => {
     if (playMusic == false){
         volumeUpImg.style.display = 'block'
         volumeMuteImg.style.display = 'none'
+        backgroundMusic.volume = 0.07
         backgroundMusic.play()
         playMusic = true
     }
      else   {
         volumeUpImg.style.display = 'none'
         volumeMuteImg.style.display = 'block'
-        backgroundMusic.currentTime = 0
-        backgroundMusic.pause()
+        backgroundMusic.volume = 0
         playMusic = false
     }
 }
@@ -48,41 +52,140 @@ backgroundMusic.onended = () => {
     
 }
 
-exitGameBtn.onclick = function(){
+ let extGmCl = () => {
     exitGameWindow.style.display = 'block'
     exitGameWindow.classList.add('appearance')
+    if(exitGameWindow.style.display == 'block'){
+        restartGameBtn.onclick = 'none'
+    }
+
 }
+
+exitGameBtn.onclick = extGmCl
+
 exitGameCansel.onclick = () =>{
     exitGameWindow.style.display = 'none'
-    
+    restartGameBtn.onclick = restGmCl    
 }
+
 exitGameOk.onclick = () =>{
+
+    if(restartGameBtn.style.width == '13vh'){
+        exitGameBtn.style.display = 'none'
+        restartGameBtn.style.display = 'none'
+        title.classList.add('appearance')
+
+        setTimeout(() => {
+            titleNormal()
+        }, 450)
+    
+        setTimeout(() => {
+            title.style.top = '22%'
+        }, 450.1)
+    }
+    else{
+        exitGameBtn.classList.add('fadeOutAnimacion')
+        restartGameBtn.classList.add('fadeOutAnimacion')
+        title.style.top = '22%'
+    }
+
     exitGameWindow.style.display = 'none'
     playZone.style.display = 'none'
-    exitGameBtn.style.display = 'none'
-    restartGameBtn.style.display = 'none'
     playBtn.style.display = 'flex'
-    title.style.top = '22%'
     divSizeBorder.style.display = 'block'
+    restartGameBtn.onclick = restGmCl
+    titleGameOver.classList.add('fadeOutAnimacion')
+
     createField()
+
+    setTimeout(() => {
+        exitGameBtn.style.display = 'none'
+        exitGameBtn.classList.remove('fadeOutAnimacion')
+        restartGameBtn.style.display = 'none'
+        restartGameBtn.classList.remove('fadeOutAnimacion')
+        titleGameOver.classList.remove('fadeOutAnimacion')
+
+        btnStyleStandart()
+
+    }, 500)
 
 }
 
-restartGameBtn.onclick = () => {
+let restGmCl = () => {
     restartGameWindow.style.display = 'block'
     restartGameWindow.classList.add('appearance')
+    if(restartGameWindow.style.display == 'block'){
+        exitGameBtn.onclick = 'none'
+    }
 }
 
+restartGameBtn.onclick = restGmCl
+
 restartGameOk.onclick = () => {
+
+    if(restartGameBtn.style.width == '13vh'){
+        fch = true
+    }
+
+    if(fch == true && restartGameBtn.style.width == '13vh' ){
+        restartGameBtn.style.display = 'none'
+        exitGameBtn.style.display = 'none'
+
+        setTimeout(() => {
+            restartGameBtn.style.display = 'block'
+            exitGameBtn.style.display = 'block'
+            fch = false
+        }, 1)
+    }
+
+    
+
     createField()
     playGame()
+    titleNormal()
     restartGameWindow.style.display = 'none'
+    exitGameBtn.onclick = extGmCl
+    
+    
+   
 }
 
 restartGameCansel.onclick = () => {
     restartGameWindow.style.display = 'none'
+    exitGameBtn.onclick = extGmCl
+
 }
 
+let gameOver = () => {
+
+    title.classList.add('fadeOutAnimacion')
+    titleGameOver.classList.add('appearance')
+    titleGameOver.style.display = 'block'
+
+    setTimeout(() => {
+        title.style.display = 'none'
+        title.classList.remove('fadeOutAnimacion')
+        playZone.classList.remove('appearance')
+    }, 500)    
+
+    setTimeout(() => {
+        playZone.classList.add('fadeOutAnimacion')
+        exitGameBtn.classList.add('fadeOutAnimacion')
+        restartGameBtn.classList.add('fadeOutAnimacion')
+    }, 1000)
+
+    setTimeout(() => {
+        btnStyleGameOver()
+        playZone.style.display = 'none'
+        playZone.classList.remove('fadeOutAnimacion')
+    }, 1450)
+    
+    setTimeout(() => {
+        exitGameBtn.classList.remove('fadeOutAnimacion')
+        restartGameBtn.classList.remove('fadeOutAnimacion')
+    }, 1500)
+    
+}
 
 let cels = 49
 let numRows = 7
@@ -91,11 +194,15 @@ let divSizeNum = 8
 let numBomb = 9
 let flagSize = 8
 let bombSize = 7.5
+let fch = false
 
-cels = 50
 let formatChanges = () => {
 
-    title.style.top = '10%'
+    if(fch == false){
+        exitGameBtn.style.display = 'block'
+        restartGameBtn.style.display = 'block'
+    }
+    title.style.top = '12.5%'
     playBtn.style.display = 'none'
     playZone.style.display = 'flex'
     playZone.style.opacity = 1
@@ -103,7 +210,7 @@ let formatChanges = () => {
     playZone.classList.add('appearance')
     exitGameBtn.classList.add('appearance')
     restartGameBtn.classList.add('appearance')
-
+    
 }
 
 let inputData = () => {
@@ -141,8 +248,6 @@ let inputData = () => {
         numBomb = 17
     }
 }
-
-
 
 let flagSiz = (inp, size, display) => {
    inp.innerHTML = ` <img id="redFlag" class="redFlag" src="images/redFlag.png" alt="" style="display: ${display}; width: ${size}vh; height: ${size}vh;"></img> `
@@ -207,10 +312,8 @@ let bombIndex = () => {
             bombSiz(zone, bombIndex, bombSize)
             zone[bombIndex].onclick = () => { 
             zone[bombIndex].innerHTML = `<img class="bomb" id="mine1" src="images/mine.png" style="display: block; width: ${bombSize}vh; height: ${bombSize}vh;" alt="bomb">` 
-            zone[bombIndex].onclick = () => {
-                alert('Бабах')
-            }
         }
+        zone[bombIndex].onclick = gameOver
         }
     }
 }
@@ -220,32 +323,57 @@ const playGame = () => {
     formatChanges()
     creatingPlayArea()  
     bombIndex()
-    exitGameBtn.style.display = 'block'
-    restartGameBtn.style.display = 'block'
+    btnStyleStandart()
+   
  }
+
  playBtn.onclick = playGame
 
-// Создаем двумерный массив
-let matrix = [];
-for (let i = 0; i < 5; i++) {
-  matrix[i] = [];
+let btnStyleGameOver = () => {
+    exitGameBtn.style.transition = '0s'
+    exitGameBtn.style.position = 'static'
+    exitGameBtn.style.order = '2'
+    exitGameBtn.style.marginLeft = '7px'
+    exitGameBtn.style.width = '13vh'
+    exitGameBtn.style.height = '13vh'
+    logOutImg.style.width = '10vh'
+    logOutImg.style.height = '10vh'
+
+    restartGameBtn.style.order = '1'
+    restartGameBtn.style.position = 'static'
+    restartGameBtn.style.marginRight = '7px'
+    restartGameBtn.style.width = '13vh'
+    restartGameBtn.style.height = '13vh'
+    restartGameBtn.style.transition = '0s'
+    repeatImg.style.height = '10vh'
+    repeatImg.style.width = '10vh'
+
+
 }
 
-// Заполняем массив числами по порядку
-let num = 1;
-for (let i = 0; i < 5; i++) {
-  for (let j = 0; j < 5; j++) {
-    matrix[i][j] = num;
-    num++;
-  }
+let btnStyleStandart = () => {
+    exitGameBtn.style.position = 'absolute'
+    exitGameBtn.style.margin = '0'
+    exitGameBtn.style.top = '5%'
+    exitGameBtn.style.left = '5%'
+    exitGameBtn.style.width = '7.5vh'
+    exitGameBtn.style.height = '7.5vh'
+    logOutImg.style.width = '5.7vh'
+    logOutImg.style.height = '5.7vh'
+
+    restartGameBtn.style.top = '13%'
+    restartGameBtn.style.left = '5%'
+    restartGameBtn.style.position = 'absolute'
+    restartGameBtn.style.width = '7.5vh'
+    restartGameBtn.style.height = '7.5vh'
+    repeatImg.style.height = '5.7vh'
+    repeatImg.style.width = '5.7vh'
 }
 
-// Создаем поле из дивов и заполняем их числами из двумерного массива
-let container = document.getElementById('zone'); // заменить 'container' на id вашего контейнера
-for (let i = 0; i < 5; i++) {
-  for (let j = 0; j < 5; j++) {
-    newDivs.innerText = matrix[i][j];
-    container.appendChild(newDivs);
-  }
+let titleNormal = () => {
+    title.style.display = 'block'
+    title.style.top = '12.5%'
+
+    titleGameOver.style.display = 'none'
 }
 
